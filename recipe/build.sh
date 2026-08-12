809a1c3675900141197dba8752418665f1ddbd2d
UENO, M. <eunosium-1128@live.jp>
2026-08-12 16:38:11 +0900

    Modify Windows DLL naming in build script
    Updated the build script to avoid encoding the libtool ABI version in the Windows DLL filename.

--- recipe/build.sh
+++ recipe/build.sh
@@ -209,8 +209,8 @@ const struct poptOption * poptGetHelpOptions(void)\
       'POPT_DATA\|poptGetAliasOptions\|poptGetHelpOptions' \
       src/popt.h src/popthelp.c || true
 
-    # Use an unversioned DLL name on Windows.
-    sed -i 's/^\(libpopt_la_LDFLAGS[[:space:]]*=.*\)$/\1 -avoid-version/' src/Makefile.in
+    # Do not encode the libtool ABI version in the Windows DLL filename.
+    sed -i 's/^[[:space:]]*libpopt_la_LDFLAGS += -version-info 0:2:0$/libpopt_la_LDFLAGS += -avoid-version/' src/Makefile.in
 fi
 
 ./configure --prefix=${PREFIX} --disable-debug --disable-dependency-tracking --disable-static
